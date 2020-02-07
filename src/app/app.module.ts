56ea07d794a119b1c398b6ddc78cf74576ab60d3
scripting7 <tonikim336@gmail.com>
2020-02-07 23:39:22 +0100

    ajouter le module de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitComponent} from './produit/produit.component';
+import { ProduitMockService} from './produit/produit.mock.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProduitMockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,21 @@
+import {Component,OnInit} from '@angular/core';
+import {ProduitMockService} from './produit.mock.service';
+import {Produit} from '../shared/produit';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.Component.html',
+  styleUrls: ['./produit.Component.css']
+})
+export class ProduitComponent implements OnInit{
+  produits: Produit[];
+
+  constructor(private produitService: ProduitMockService){
+
+
+  }
+  ngOnInit(){
+    this.produits=this.produitService.getProduits();
+  }
+
+}
